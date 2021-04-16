8ce80b740ce3e28c4f6d7f147f2d30038ab2dd5b
Evialina <evialina@protonmail.com>
2021-04-16 16:14:05 +0100

    Update term collector process, MySQL schema generation, and docker config

--- docker/db/init/init.sql
+++ docker/db/init/init.sql
@@ -1,19 +1,7 @@
--- Host: localhost:8889
--- Generation Time: Apr 02, 2021 at 01:12 PM
--- Server version: 5.7.32
--- PHP Version: 7.4.12
-
---
--- Database: `doclab`
---
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 CREATE DATABASE IF NOT EXISTS `doclab` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 USE `doclab`;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `authors`
---
 
 CREATE TABLE `authors` (
   `id` int(11) UNSIGNED NOT NULL,
@@ -22,49 +10,50 @@ CREATE TABLE `authors` (
   `locked` tinyint(1) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `documents`
---
+CREATE TABLE `dictionary` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `document_id` int(11) NOT NULL,
+  `term` tinytext NOT NULL,
+  `term_frequency` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `documents` (
   `id` int(11) NOT NULL,
   `url` text NOT NULL,
   `content` longtext,
-  `indexed` tinyint(1) DEFAULT '0',
   `last_crawl` date DEFAULT NULL,
-  `locked` tinyint(1) NOT NULL DEFAULT '0'
+  `locked` tinyint(1) NOT NULL DEFAULT '0',
+  `indexed` tinyint(1) DEFAULT '0',
+  `index_locked` tinyint(1) DEFAULT '0',
+  `title` mediumtext,
+  `description` mediumtext,
+  `authors` json DEFAULT NULL,
+  `topic` varchar(20) DEFAULT '',
+  `screenshot` varchar(35) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Indexes for dumped tables
---
 
---
--- Indexes for table `authors`
---
 ALTER TABLE `authors`
   ADD PRIMARY KEY (`id`);
 
---
--- Indexes for table `documents`
---
+ALTER TABLE `dictionary`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `document_id` (`document_id`);
+
 ALTER TABLE `documents`
   ADD PRIMARY KEY (`id`);
+ALTER TABLE `documents` ADD FULLTEXT KEY `title` (`title`,`description`);
 
---
--- AUTO_INCREMENT for dumped tables
---
 
---
--- AUTO_INCREMENT for table `authors`
---
 ALTER TABLE `authors`
   MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT for table `documents`
---
+ALTER TABLE `dictionary`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+
 ALTER TABLE `documents`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+
+ALTER TABLE `dictionary`
+  ADD CONSTRAINT `dictionary_ibfk_1` FOREIGN KEY (`document_id`) REFERENCES `documents` (`id`);
